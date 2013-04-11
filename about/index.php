f4fb7be1209eecc877053bbfc6bc786bba1672ac
Daniel Lopez <tauron32@gmail.com>
2013-04-12 01:15:07 +0200

    Arreglado problema con los enlaces al CSS

--- about/index.php
+++ about/index.php
@@ -7,8 +7,8 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>ETSII QR CHALLENGE</title>
-<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="../css/style.css">
 </head>
 <body>
 
