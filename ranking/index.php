b8c53909cfceaf4086a97e804347d2ee5ab74d99
Daniel Lopez <tauron32@gmail.com>
2013-04-12 01:18:28 +0200

    Creado estilo básico para el Ránking

--- ranking/index.php
+++ ranking/index.php
@@ -1,3 +1,36 @@
 <?php
 //	-/ranking/index.php
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>ETSII QR CHALLENGE | Ránking</title>
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+<body>
+
+  <div class="container-narrow">
+
+    <div class="masthead">
+      <ul class="nav nav-pills pull-right">
+        <li><a href="/">Inicio</a></li>
+        <li class="active"><a href="#">Ránking</a></li>
+        <li><a href="../about/">About</a></li>
+      </ul>
+      <img src="../img/logoetsii.png" width="40px" height="auto" class="alignleft" style="margin-right:3px"><h3 class="muted">ETSI Informática</h3>
+    </div>
+
+    <hr>
+
+    <hr>
+
+    <div class="footer">
+    <p>&copy; <a href="http://www.informatica.uma.es">Escuela Técnica Superior de Ingeniería Informática</a> | UMA 2013</p>
+    </div>
+
+  </div> <!-- /container -->
+</body>
+</html>
