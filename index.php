aac4dffd449087bc586a0ecf9008d00079e32608
Daniel Lopez <tauron32@gmail.com>
2013-04-11 21:50:44 +0200

    Página de inicio básica y estilo básico

--- index.php
+++ index.php
@@ -1,14 +1,62 @@
 <?php
-//	-/index.php
+//-/index.php
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="UTF-8">
-	<title>ETSII QR CHALLENGE</title>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>ETSII QR CHALLENGE</title>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
 
+	<div class="container-narrow">
+
+		<div class="masthead">
+			<ul class="nav nav-pills pull-right">
+				<li class="active"><a href="#">Inicio</a></li>
+				<li><a href="#">Ránking</a></li>
+				<li><a href="#">About</a></li>
+			</ul>
+			<h3 class="muted">ETSI Informática</h3>
+		</div>
+
+		<hr>
+
+		<div class="jumbotron">
+			<h1>QR Challenge!</h1>
+			<p class="lead">Consigue puntos escaneando los códigos QR que hay repartidos por el pabellón durante la Jornada de Puertas Abiertas.</p>
+			<a class="btn btn-large btn-success" href="#">Ver el ránking</a>
+		</div>
+
+		<hr>
+
+		<div class="row-fluid marketing">
+			<div class="span6">
+				<h4>Reglas</h4>
+				<ul>
+					<li>Escanear un stand: <strong>25 puntos</strong></li>
+					<li><em>Disparar</em> a una persona: <strong>75 puntos</strong></li>
+					<li>Encontrar una pegatina escondida: <strong>100 puntos</strong></li>
+				</ul>
+			</div>
+
+			<div class="span6">
+				<h4>Requisitos para ganar</h4>
+				<ul>
+					<li>Necesitas conseguir al menos <strong>300 puntos</strong></li>
+				</ul>
+			</div>
+		</div>
+
+		<hr>
+
+		<div class="footer">
+		<p>&copy; Escuela Técnica Superior de Ingeniería Informática | UMA 2013</p>
+		</div>
+
+	</div> <!-- /container -->
 </body>
 </html>
