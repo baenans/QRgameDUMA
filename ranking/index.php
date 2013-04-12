1ff16bc0db90d790e01e2d16a05e9c3dfd37f241
Daniel Lopez <tauron32@gmail.com>
2013-04-12 02:48:22 +0200

    Ranking medio listo

--- ranking/index.php
+++ ranking/index.php
@@ -25,7 +25,22 @@
     </div>
 
     <hr>
-
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>Posición</th>
+            <th>Nick</th>
+            <th>Puntuación</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+            foreach(scoreOfAll() as $row) {
+              echo "<tr><td>" . $row -> number . "</td><td>" . $row -> nick . "</td><td>" . $row -> score . "</td></tr>";
+            }
+          ?>
+        </tbody>
+      </table>
     <hr>
 
     <div class="footer">
