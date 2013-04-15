ca2b0467995c93daced251dbcad1e040a5cd140a
Francisco Baena <yo@baenafrancisco.com>
2013-04-15 17:33:45 +0200

    Idea de diseño para /shoot/index.php

--- shoot/index.php
+++ shoot/index.php
@@ -1,3 +1,12 @@
 <?php
 //	-/shoot/index.php
 ?>
+
+
+<P align=center>Estas a punto de disparar a @TALPERSONA<br><br>
+
+
+Para confirmar, pulsa 
+<!-- boton -->
+<br>AQU&Iacute;</p> <!-- (si es un iPhone, no aparecerá este botón, sino que para confirmar debes abrir con Safari)-->
+
