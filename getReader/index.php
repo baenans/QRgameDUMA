52c28fdd1aefd5c48f10fc04fdda9a723055bde9
Francisco Baena <yo@baenafrancisco.com>
2013-04-15 19:21:51 +0200

    Añadida compatibilidad BB

--- getReader/index.php
+++ getReader/index.php
@@ -1,6 +1,7 @@
 <?php
 //	-/getReader/index.php
-	$agent = $_SERVER['HTTP_USER_AGENT'];  
+	$agent = $_SERVER['HTTP_USER_AGENT']; 
+	//echo $agent; 
 	if(preg_match('/iPhone/i', $agent)){  
 		header("Location: https://itunes.apple.com/us/app/qr-reader-for-iphone/id368494609?mt=8");
 	} else if (preg_match('/Android/i', $agent)) {
@@ -9,6 +10,8 @@
 		header("Location: http://www.windowsphone.com/en-us/store/app/qr-code-offline/3a156888-2f6d-4bad-89f9-fc071a820435");
 	} else if (preg_match('/Blackberry/i', $agent)) {
 		header("Location: http://appworld.blackberry.com/webstore/content/118628/?lang=en&countrycode=ES");
+	} else if (preg_match('/BB/i', $agent)) {
+		header("Location: http://http://appworld.blackberry.com/webstore/content/48078/?lang=es&countrycode=ES");
 	}
 ?>
 <!doctype html>
