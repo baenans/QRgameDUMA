8ff07932349f050490c6fb2eab5f0c136e26d638
Daniel Lopez <tauron32@gmail.com>
2013-04-16 12:47:25 +0200

    Haciendo responsive el index.php

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>ETSII QR CHALLENGE</title>
-<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="../css/bootstrap-responsive.css">
 <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
@@ -20,7 +21,9 @@
 				<li><a href="ranking/">Ránking</a></li>
 				<li><a href="about/">About</a></li>
 			</ul>
-			<img src="img/logoetsii.png" width="40px" height="auto" class="alignleft" style="margin-right:3px"><h3 class="muted">ETSI Informática</h3>
+			<div class="logo">
+				<img src="img/logoetsii.png" width="40px" height="auto" class="alignleft" style="margin-right:3px"><h3 class="muted">ETSI Informática</h3>
+			</div>
 		</div>
 
 		<hr>
